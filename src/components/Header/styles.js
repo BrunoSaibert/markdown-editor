0f94835a3239924ab993b1b992e25948926d8de6
BrunoSaibert <brunosaibert@gmail.com>
2020-07-12 17:23:12 -0300

    Estilo Header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,22 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.header`
+  width: 100%;
+  height: 70px;
+  background: #1b9;
+  padding: 0 4%;
+  margin-bottom: 50px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Brand = styled.a`
+  color: #fff;
+  text-transform: lowercase;
+  text-decoration: none;
+  font-size: 24px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
